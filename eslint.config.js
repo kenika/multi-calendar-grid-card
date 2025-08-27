3abefc95f4602acabc8b52707f3e24ec51f8f3f8
kenika <33599525+kenika@users.noreply.github.com>
2025-08-27 11:55:04 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,18 +2,17 @@
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  // Ignore build outputs and deps
   { ignores: ["dist/**", "node_modules/**"] },
-
-  // TypeScript recommended rules
   ...tseslint.configs.recommended,
-
-  // Our tweaks
   {
     files: ["**/*.{ts,js}"],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/ban-ts-comment": "off"
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrors: "none" }
+      ]
     }
   }
 );
